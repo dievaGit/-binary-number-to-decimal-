56826edd7b7d7d15f4b97f23c2b4a212aa24e22c
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-20 13:18:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Tarea8Cap8
+{
+    class Program
+    {//Convertir de decimal a binario
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("Enter one number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int newNumber = n;
+
+            Stack binary = new Stack();
+
+            while(newNumber != 1)
+            {
+                binary.Push(newNumber % 2);
+                newNumber = newNumber / 2;
+                if(newNumber == 1)
+                {
+                    binary.Push(newNumber);
+                }
+            }
+
+            int number = 0;
+            while(binary.Count != 0)
+            {
+                number = (int)binary.Pop();
+                Console.Write("{0}", number);
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
